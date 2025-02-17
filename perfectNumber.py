3b49eed7a9c59842dfec5294d8ebe69962348442
Abel Ikhaguebor <B00142713@mytudublin.ie>
2025-02-17 17:46:22 +0000

    Completed Step 2

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,20 +1,27 @@
-#Add your name here
-#Add the date here
-#Add the filename here
+#Abel Ikhaguebor
+#17/02/2025
+#perfectNumber.py
 
 #A perfect number is one for which all the divisors of the number add up to the
 #number itself. For example the divisors of 28 are 1,2,4,7,14 which added together gives 28
 #write a function below called perfectNumber(x) which checks to see if x is a perfect number
+def isPerfectNumber(n):
+    divisors_list = []
+    for i in range (1,n):
+        if n % i == 0:
+            divisors_list.append(i)
+    return divisors_list
+
+    
 #The function should return True if the number is perfect and False if it is not
 
-#from divisors import divisors
-
+from divisors import divisors
 
 #define the function header called perfectNumber expecting one argument
 
 
     #set a result variable to False by default
-
+  
 
     #if the sum of all the divisors of the number is equal to the test number
 
@@ -22,8 +29,13 @@
  
 
     #return the result variable
-
+   
 
 #uncomment the following line to test the perfectNumber function it should return True
-#if (perfectNumber(8128)): print("8128 is a perfect number")
+        if (perfectNumber(8128)):
+    return True
+        else
+    return False
+
+print("8128 is a perfect number")
 
